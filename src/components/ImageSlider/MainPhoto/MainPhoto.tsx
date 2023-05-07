86aa7319b0a812ff6e0441095bf425dbd20091dd
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-07 15:26:02 +0300

    update MainPhoto

--- src/components/ImageSlider/MainPhoto/MainPhoto.tsx
+++ src/components/ImageSlider/MainPhoto/MainPhoto.tsx
@@ -7,15 +7,31 @@ interface MainPhotoProps {
 }
 
 function MainPhoto({ photos, currentPhotoIndex }: MainPhotoProps) {
+  function handleCurrentDataset(index: number) {
+    const prevPhotoIndex = currentPhotoIndex - 1;
+    const nextPhotoIndex = currentPhotoIndex + 1;
+
+    if (prevPhotoIndex === index || nextPhotoIndex === index) {
+      return 'prepared';
+    }
+
+    if (currentPhotoIndex === index) {
+      return 'true';
+    }
+
+    return 'false';
+  }
+
   return (
     <div className={styles.mainPhoto}>
       {photos.map((photo, index) => (
         <img
           key={index}
           className={styles.photo}
-          data-current={index === currentPhotoIndex}
+          data-current={handleCurrentDataset(index)}
           src={photo.src}
           alt={photo.description}
+          loading="lazy"
         />
       ))}
     </div>
